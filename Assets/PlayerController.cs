11b997d8c30543877dc85eadd0fa4d871dae880d
Dejan Popov <dekipopica92@gmail.com>
2020-10-06 16:26:58 +0200

    Made character and gun

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] 
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3;
+
+    private PlayerMotor motor;
+
     void Start()
     {
-        
+        motor = GetComponent<PlayerMotor>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+
+        // 2 vectors
+        Vector3 moveHorizontal = transform.right * xMove;  
+        Vector3 moveVertical = transform.forward * zMove;
+
+        // Final movement vector
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        // Apply movement
+        motor.Move(velocity);
+
+        //Calculate rotation as 3D vector
+        //Mouse rotation left and right
+        //Uvek ocemo da Mouse X utice na pleyera a da Mouse Y utice na kameru.
+        float yRotation = Input.GetAxisRaw("Mouse X");
+
+        Vector3 rotation = new Vector3(0f, yRotation, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.Rotation(rotation);
+
+        //Calculate camera rotation as a 3D vector
+        float xRotation = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 cameraRotation = new Vector3(xRotation , 0f, 0f) * lookSensitivity;
+
+        //Apply camera rotation
+        motor.RotateCamera(cameraRotation);
     }
 }
--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -2,17 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))] 
+
 public class PlayerMotor : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocityZ = Vector3.zero;
+    private Vector3 rotationZ = Vector3.zero;
+    private Vector3 cameraRotationZ = Vector3.zero;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void Move (Vector3 velocity)
     {
-        
+        velocityZ = velocity;
+    }
+
+    //Gets rotational vector
+    public void Rotation(Vector3 rotation)
+    {
+        rotationZ = rotation;
+    }
+
+    public void RotateCamera(Vector3 cameraRotation)
+    {
+        cameraRotationZ = cameraRotation;
+    }
+
+
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    void PerformMovement()
+    {
+        if(velocityZ != Vector3.zero) 
+        {
+            rb.MovePosition(rb.position + velocityZ * Time.fixedDeltaTime);
+        }
+    }
+    //Perform rotation
+    void PerformRotation()
+    {
+        // Quaternion.Euler vraca rotaciju koja se rotira x stepeni oko x ose, y stepeni oko y ose
+        // i z stepeni oko z ose
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotationZ));
+
+        if (cam != null)
+        {
+            cam.transform.Rotate(-cameraRotationZ);
+        }
     }
 }
