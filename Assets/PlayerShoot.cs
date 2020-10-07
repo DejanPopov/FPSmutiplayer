57686183011e970e73478f06f89657debe4f0c22
Dejan Popov <dekipopica92@gmail.com>
2020-10-07 18:12:10 +0200

    Weapon and Shoot scripts
    -Added stuff on playground

---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+using Mirror;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    public PlayerWeapon weapon;
+    
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private LayerMask mask;
+
+    private void Start()
+    {
+        if (cam == null)
+        {
+            Debug.LogError("PlayerShoot: No camera referenced!");
+            this.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, 
+            cam.transform.forward, out hit, 
+            weapon.range, mask))
+        {
+            Debug.Log("We hit" + hit.collider.name);
+        }
+    }
+}
---
+++ Assets/PlayerWeapon.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+
+[System.Serializable]  //Unity ce znati kako da sacuva ovu klasu.
+public class PlayerWeapon
+{
+    public string name = "Rifle";
+    public float damage = 10f;
+    public float range = 100f;
+
+}
