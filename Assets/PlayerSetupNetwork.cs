7b8cedfd8f0023a35cf701fd9f868ccc5d8709bd
Dejan Popov <dekipopica92@gmail.com>
2020-10-07 12:04:47 +0200

    Networking

---
+++ Assets/PlayerSetupNetwork.cs
@@ -0,0 +1,22 @@
+﻿using Mirror;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetupNetwork : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    private void Start()
+    {
+        //Ovde proveravamo da li kontrolisemo igraca.Ako ga ne kontrolisemo onda ce on da ugasi sve 
+        //komponente (u smislu da se prikljucio drugi igrac,da se ne kopiraju potezi)
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+    }
+}
